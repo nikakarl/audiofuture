89a50a58d535ee9d5ef54e25c71411dbf02fc09d
nikakarl <nika.karliuchenko@gmail.com>
2025-05-31 12:14:43 -0400

    fixing node

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  publicDir: 'public',
+  publicDir: "public",
   build: {
-    outDir: 'dist',
+    outDir: "dist",
   },
   server: {
     port: 8080,
   },
-  define: {
-    __WS_TOKEN__: JSON.stringify(''),
-  },
-})
+});
